f36addcdcc80019ff185784bd3054ca0285c45de
shiun <alen566@gmail.com>
2019-09-14 16:10:32 +0800

    * Use product directory in HOST instead of use docker volume * Allow restart django project by touch uwsgi file

--- init_service.sh
+++ init_service.sh
@@ -31,30 +31,35 @@ echo "[DB] Generate docker-compose for postgres success"
 # ==================================================
 # Generate docker files for Django project - start
 # ==================================================
-DJANGO_PROJECT_DIR="`pwd`/django_project"
+DJANGO_PROJECT_DIR_NAME="django_project"
+DJANGO_PROJECT_DIR="`pwd`/${DJANGO_PROJECT_DIR_NAME}"
 echo "[Django] Generate docker-compose for django project..."
 
 # Copy docker-compose example and set container name of nginx
-sed "s/nginx-container/${DJANGO_PROJECT_NAME}-nginx-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml.example > ${DJANGO_PROJECT_DIR}/docker-compose.yml
+sed "s/nginx-container/${CONTAINER_PREFIX}-${DJANGO_PROJECT_NAME}-nginx-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml.example > ${DJANGO_PROJECT_DIR}/docker-compose.yml
 
 # Set container name of django project 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sed -i "s/web-container/${DJANGO_PROJECT_NAME}-web-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i "s/web-container/${CONTAINER_PREFIX}-${DJANGO_PROJECT_NAME}-web-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/web-container/${DJANGO_PROJECT_NAME}-web-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i '' "s/web-container/${CONTAINER_PREFIX}-${DJANGO_PROJECT_NAME}-web-container/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
 fi
 echo "[Django] Set container name success"
 
 # Copy neccesary configs from eample of docker and django files such as Dockerfile, docker-entrypoint and uwsgi, ...etc.
 cp -r ${DJANGO_PROJECT_DIR}/docker_example/* ${DJANGO_PROJECT_DIR}/${DJANGO_PROJECT_NAME}/
+chmod +x ${DJANGO_PROJECT_DIR}/${DJANGO_PROJECT_NAME}/docker-entrypoint.sh
 
 # Set up run time name of container, volume and HOST name
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # Volume name
-    sed -i "s/project_data/${DJANGO_PROJECT_NAME}-project_data/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i "s+project_data+./${DJANGO_PROJECT_NAME}+g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+
+    # Setup variables of for django build
+    sed -i "s+build:\ web_project+build:\ ./${DJANGO_PROJECT_NAME}+g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
 
     # Setup variables according to project name of django
-    sed -i "s/web_project/${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i "s/web_project/${CONTAINER_PREFIX}_${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
     sed -i "s/web_project/${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/${DJANGO_PROJECT_NAME}/uwsgi.ini
 
     # Setup variables of HOST
@@ -65,10 +70,13 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sed -i "s/LETSENCRYPT_EMAIL\=\S*/LETSENCRYPT_EMAIL=${MAINTAINER}/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # Volume name
-    sed -i '' "s/project_data/${DJANGO_PROJECT_NAME}-project_data/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i '' "s+project_data+./${DJANGO_PROJECT_NAME}+g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+
+    # Setup variables of for django build
+    sed -i '' "s+build:\ web_project+build:\ ./${DJANGO_PROJECT_NAME}+g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
 
     # Setup variables according to project name of django
-    sed -i '' "s/web_project/${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
+    sed -i '' "s/web_project/${CONTAINER_PREFIX}_${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/docker-compose.yml
     sed -i '' "s/web_project/${DJANGO_PROJECT_NAME}/g" ${DJANGO_PROJECT_DIR}/${DJANGO_PROJECT_NAME}/uwsgi.ini
 
     # Setup variables of HOST
